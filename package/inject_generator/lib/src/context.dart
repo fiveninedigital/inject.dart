6bbff3c607e4765cdafbf11438b4fd862f58264c
Jan Humbel <jan@fivenine.ch>
2020-02-25 17:16:42 +0100

    update deprecated api

--- package/inject_generator/lib/src/context.dart
+++ package/inject_generator/lib/src/context.dart
@@ -103,7 +103,7 @@ class BuilderLogger {
     // <TRANSITIONAL_API>
     ElementDeclarationResult elementDeclaration;
     if (element.kind != ElementKind.DYNAMIC) {
-      var parsedLibrary = ParsedLibraryResultImpl.tmp(element.library);
+      var parsedLibrary = AnalysisSession.getParsedLibraryByElement(element.library);
       if (parsedLibrary.state == ResultState.VALID) {
         elementDeclaration = parsedLibrary.getElementDeclaration(element);
       }
